0256e907ed137c0ca743963c8d0923b6401b6cf6
bark20 <109058939+bark20@users.noreply.github.com>
2023-05-08 01:41:31 +0900

    Update item.py

--- item.py
+++ item.py
@@ -142,8 +142,8 @@ aac = aac.rename(columns={'index': '투척 무기'})
 bb = bb.rename(columns={'index': '사용한 회복템'})
 cc = cc.rename(columns={'index': '사용한 투척 무기'})
 aac = aac.reset_index(drop=True)
-aab = aab.iloc[1:,:]
-aac = aac.iloc[1:,:]
+# aab = aab.iloc[1:,:]
+# aac = aac.iloc[1:,:]
 e = pd.concat([aab.iloc[:,:1],bb/aab * 100], axis=1).iloc[:,:-2]
 f = pd.concat([aac.iloc[:,:1],cc/aac * 100], axis=1).iloc[:,:-2]
 e = e.rename(columns = {"회복템" : "회복템 사용률"})
