e01b6a977b19c787be1c416b39889322e5aff48b
bark20 <109058939+bark20@users.noreply.github.com>
2023-05-08 01:23:20 +0900

    Update item.py

--- item.py
+++ item.py
@@ -142,10 +142,12 @@ aac = aac.rename(columns={'index': '투척 무기'})
 bb = bb.rename(columns={'index': '사용한 회복템'})
 cc = cc.rename(columns={'index': '사용한 투척 무기'})
 aac = aac.reset_index(drop=True)
-# e = pd.concat([aab.iloc[:,:1],bb/aab * 100], axis=1).iloc[:,:-2]
-# f = pd.concat([aac.iloc[:,:1],cc/aac * 100], axis=1).iloc[:,:-2]
-# e = e.rename(columns = {"회복템" : "회복템 사용률"})
-# f = f.rename(columns = {"투척 무기" : "투척 무기 사용률"})
+aab = aab.iloc[1:,:]
+aac = aac.iloc[1:,:]
+e = pd.concat([aab.iloc[:,:1],bb/aab * 100], axis=1).iloc[:,:-2]
+f = pd.concat([aac.iloc[:,:1],cc/aac * 100], axis=1).iloc[:,:-2]
+e = e.rename(columns = {"회복템" : "회복템 사용률"})
+f = f.rename(columns = {"투척 무기" : "투척 무기 사용률"})
 
 
 # 팀 선택
@@ -155,18 +157,18 @@ team_name = st.selectbox("팀 선택", ('17', '4AM', 'ACE', 'CES', 'DAY', 'DNW',
 
 choice = st.selectbox("보고 싶은 그래프", ('총 얻은 아이템', '총 사용한 아이템'))
 # , '아이템 사용률'
-A = aab#.sort_values(team_name, ascending=False)
-B = aac#.sort_values(team_name, ascending=False)
-C = bb#.sort_values(team_name, ascending=False)
-D = cc#.sort_values(team_name, ascending=False)
-# E = e.sort_values(team_name, ascending=False)
-# F = f.sort_values(team_name, ascending=False)
+A = aab.sort_values(team_name, ascending=False)
+B = aac.sort_values(team_name, ascending=False)
+C = bb.sort_values(team_name, ascending=False)
+D = cc.sort_values(team_name, ascending=False)
+E = e.sort_values(team_name, ascending=False)
+F = f.sort_values(team_name, ascending=False)
 AA = A[["회복템",team_name]]
 BB = B[["투척 무기",team_name]]
 CC = C[["사용한 회복템",team_name]]
 DD = D[["사용한 투척 무기",team_name]]
-# EE = E[["회복템 사용률",team_name]]
-# FF = F[["투척 무기 사용률",team_name]]
+EE = E[["회복템 사용률",team_name]]
+FF = F[["투척 무기 사용률",team_name]]
 def plot_item():
     if choice =='총 얻은 아이템':
     # 각 차트를 시각화하고 웹 앱으로 만들기
@@ -180,8 +182,7 @@ def plot_item():
         fig, ax = plt.subplots()
         sns.barplot(data=B, x=team_name, y='투척 무기', palette='YlOrBr', ax=ax)
         st.pyplot(fig)
-    else:
-#     elif choice == '총 사용한 아이템':
+    elif choice == '총 사용한 아이템':
         st.dataframe(CC, use_container_width=True)
         fig, ax = plt.subplots()
         sns.barplot(data=C, x=team_name, y='사용한 회복템', palette='YlOrBr', ax=ax)
@@ -191,14 +192,14 @@ def plot_item():
         fig, ax = plt.subplots()
         sns.barplot(data=D, x=team_name, y='사용한 투척 무기', palette='YlOrBr', ax=ax)
         st.pyplot(fig)
-#     else:
-#         st.dataframe(EE, use_container_width=True)
-#         fig, ax = plt.subplots()
-#         sns.barplot(data=E, x=team_name, y='회복템 사용률', palette='YlOrBr', ax=ax)
-#         st.pyplot(fig)
+    else:
+        st.dataframe(EE, use_container_width=True)
+        fig, ax = plt.subplots()
+        sns.barplot(data=E, x=team_name, y='회복템 사용률', palette='YlOrBr', ax=ax)
+        st.pyplot(fig)
 
-#         st.dataframe(FF, use_container_width=True)
-#         fig, ax = plt.subplots()
-#         sns.barplot(data=F, x=team_name, y='투척 무기 사용률', palette='YlOrBr', ax=ax)
-#         st.pyplot(fig)
+        st.dataframe(FF, use_container_width=True)
+        fig, ax = plt.subplots()
+        sns.barplot(data=F, x=team_name, y='투척 무기 사용률', palette='YlOrBr', ax=ax)
+        st.pyplot(fig)
 plot_item()
