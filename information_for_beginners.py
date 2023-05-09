4b77d9731cdf7bd5c0017e1e17a1ec7b6bdc62bd
bark20 <109058939+bark20@users.noreply.github.com>
2023-05-09 14:31:24 +0900

    Update information_for_beginners.py

--- information_for_beginners.py
+++ information_for_beginners.py
@@ -7,8 +7,8 @@ import plotly.express as px
 import os
 
 st.set_page_config(page_title="BattleGround_Final", page_icon=":guardsman:", layout="wide")
-st.title(':scroll: 배틀그라운드: 초보자 꿀 정보 :scroll:')
-st.header('WWCD 초보자 꿀 정보 오신 것을 환영합니다! :sparkles:')
+st.title(':scroll: 배틀그라운드: 경기 정보 :scroll:')
+st.header('WWCD 경기 정보 오신 것을 환영합니다! :sparkles:')
 st.markdown(' ')
 st.markdown(' ')
 
@@ -18,7 +18,7 @@ st.markdown(' **PUBG 글로벌 시리즈의 그 첫 번째 대회, PGS 1**이 �
 st.markdown(" PGS 1은 4월 27일부터 5월 7일까지의 일정으로 진행되며, 그룹 스테이지, 승자/패자 브래킷 그리고 그랜드 파이널까지의 여정으로 구성되어 있는데요,")
 st.markdown(' PGS 1은 총 24개의 탑 티어 글로벌 팀들이 Battle Arena Malaysia 경기장에 모여 총상금 $500,000을 놓고 격돌하게 됩니다!')
 st.markdown(' 경기에서 진행될 맵은 에란겔과 미라마, 두 개의 맵입니다.')
-st.markdown(" 4월 27일부터 29일까지의 총 18번의 경기에서 얻어온 70만 개가 넘는 다양한 로그 데이터를 분석하여 신규 유저분들에게 도움이 되고자, PGS 1에서 얻은 바탕으로 초보자위한 꿀 정보를 만들어 보았습니다.")
+st.markdown(" 4월 27일부터 29일까지의 총 18번의 경기에서 얻어온 70만 개가 넘는 다양한 로그 데이터를 분석하여 신규 유저분들에게 도움이 되고자, PGS 1에서 얻은 바탕으로 경기 정보를 만들어 보았습니다.")
 st.markdown(' ')
 st.markdown(' ')
 df_move_distance_mean = pd.read_csv("https://raw.githubusercontent.com/AIS8-WWCD/streamlit_dashboard/main/pages/df_move_distance_mean.csv")
